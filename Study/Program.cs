c97a8ce32ea60da072b40762ff39a72b6c1f69c6
ykchoi99 <choe_gang@naver.com>
2021-07-08 00:26:34 +0900

    this

--- Study/Program.cs
+++ Study/Program.cs
@@ -4,32 +4,47 @@ namespace Study
 {
     class Program
     {
-        class MyClass {
+        class MyClass
+        {
             public int MyField1;
             public int MyField2;
             public MyClass DeepCopy()
             {
-                MyClass newCopy = new MyClass();
-                newCopy.MyField1 = this.MyField1;
-                newCopy.MyField2 = this.MyField2;
+                MyClass newCopy = new()
+                {
+
+                    MyField1 = this.MyField1,
+                    MyField2 = this.MyField2
+                };
                 return newCopy;
             }
         }
-
-        class MyClass2
+        class Employee
         {
-            public int MyField1;
-            public int MyField2;
-        }
 
+            private string Name;
+            private string Position;
+
+            public void SetName(string Name)
+            {
+                this.Name = Name;
+            }
+            public string GetName()
+            {
+                return Name;
+            }
+
+            public void SetPosition(string Position)
+            {
+                this.Position = Position;
+            }
+            public String GetPosition()
+            {
+                return Position;
+            }
+        }
         static void Main(string[] args)
         {
-            MyClass2 myc = new() { MyField1 = 10, MyField2 = 20 };
-            MyClass2 myc2 = new() { MyField1 = 30 };
-
-            Console.WriteLine("{0},{1}", myc.MyField1, myc.MyField2);
-            Console.WriteLine("{0},{1}", myc2.MyField1, myc2.MyField2);
-
             Console.WriteLine("Shallow Copy");
             {
                 MyClass source = new()
@@ -48,6 +63,37 @@ namespace Study
 
             }
 
+            Console.WriteLine("Deep Copy");
+
+            {
+                MyClass source = new()
+                {
+                    MyField1 = 10,
+                    MyField2 = 20
+                };
+
+                MyClass target = new()
+                {
+                    MyField2 = 30
+                };
+                Console.WriteLine($"{source.MyField1} {source.MyField2}");
+                Console.WriteLine($"{target.MyField1} {target.MyField2}");
+            }
+
+            Employee pooh = new Employee();
+            pooh.SetName("1");
+            pooh.SetPosition("1");
+            
+
+        Console.WriteLine($"{pooh.GetName()} {pooh.GetPosition()}");
+
+
+            Employee pooh2 = new Employee();
+            pooh2.SetName("2");
+            pooh2.SetPosition("2");
+
+            Console.WriteLine($"{pooh2.GetName()} {pooh2.GetPosition()}");
         }
     }
 }
+
