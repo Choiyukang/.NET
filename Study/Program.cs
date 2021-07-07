da9845fb1ff4d91c3642f6f757ad271b2efb0a7b
ykchoi99 <choe_gang@naver.com>
2021-07-07 23:36:43 +0900

    p.235

--- Study/Program.cs
+++ Study/Program.cs
@@ -4,9 +4,50 @@ namespace Study
 {
     class Program
     {
+        class MyClass {
+            public int MyField1;
+            public int MyField2;
+            public MyClass DeepCopy()
+            {
+                MyClass newCopy = new MyClass();
+                newCopy.MyField1 = this.MyField1;
+                newCopy.MyField2 = this.MyField2;
+                return newCopy;
+            }
+        }
+
+        class MyClass2
+        {
+            public int MyField1;
+            public int MyField2;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MyClass2 myc = new() { MyField1 = 10, MyField2 = 20 };
+            MyClass2 myc2 = new() { MyField1 = 30 };
+
+            Console.WriteLine("{0},{1}", myc.MyField1, myc.MyField2);
+            Console.WriteLine("{0},{1}", myc2.MyField1, myc2.MyField2);
+
+            Console.WriteLine("Shallow Copy");
+            {
+                MyClass source = new()
+                {
+                    MyField1 = 10,
+                    MyField2 = 20
+                };
+
+                MyClass target = new()
+                {
+                    MyField2 = 30
+                };
+
+                Console.WriteLine($"{source.MyField1} {source.MyField2}");
+                Console.WriteLine($"{target.MyField1} {target.MyField2}");
+
+            }
+
         }
     }
 }
