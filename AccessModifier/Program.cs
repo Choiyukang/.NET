97fea864eda412ec90517e10014d03b99d4bcdda
ykchoi99 <choe_gang@naver.com>
2021-07-08 23:27:38 +0900

    p.254

---
+++ AccessModifier/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace AccessModifier
+{
+    class Program
+    {
+        class WaterHeater
+        {
+            protected int temperature;
+            public void SetTemperature(int temperature)
+            {
+                if (temperature < -5 || temperature > 42)
+                    throw new Exception("Out of temperature range");
+                this.temperature = temperature;
+            }
+            internal void TurnOnwater()
+            {
+                Console.WriteLine($"turn on water : {temperature}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                WaterHeater heater = new();
+                heater.SetTemperature(20);
+                heater.TurnOnwater();
+
+                heater.SetTemperature(-2);
+                heater.TurnOnwater();
+
+                heater.SetTemperature(50);
+                heater.TurnOnwater();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        class Base
+        {
+            protected string Name;
+            public Base(string Name)
+            {
+                this.Name = Name;
+                Console.WriteLine($"{this.Name}.Base()");
+            }
+            ~Base()
+            {
+                Console.WriteLine($"{Name}.BaseMethod()");
+            }
+            public void BaseMethod()
+            {
+                Console.WriteLine($"{Name}.BaseMethod()");
+            }
+        }
+        class Deriver : Base
+        {
+            public Deriver(string Name):base(Name)
+            {
+                Console.WriteLine($"{ this.Name}.Deriver()");
+            }
+            ~Deriver()
+            {
+                Console.WriteLine($"{this.Name}.~Derived()");
+            }
+            public void DerivedMethod()
+            {
+                Console.WriteLine($"{Name}.DerivedMethod()");
+            }
+        }
+        
+
+        static void Main(string[] args)
+        {
+            Base a = new("A");
+            a.BaseMethod();
+
+            Deriver b = new("B");
+            b.BaseMethod();
+            b.DerivedMethod();
+        }
+    }
+}
