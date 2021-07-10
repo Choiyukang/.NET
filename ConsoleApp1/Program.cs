967d42de693b2619ab52790998e1bcf56c9cee08
ykchoi99 <choe_gang@naver.com>
2021-07-11 00:10:02 +0900

    p.271

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        class Base
+        {
+            public void MyMethod()
+            {
+                Console.WriteLine
+                    ("Base Mymethod()");
+            }
+        }
+
+        class Derived : Base
+        {
+            public new void MyMethod()
+            {
+                Console.WriteLine("Base Mymethod()11");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Base baseObj = new();
+            baseObj.MyMethod();
+
+            Derived de = new();
+            de.MyMethod();
+
+            Base baseDerivedr = new();
+            baseDerivedr.MyMethod();
+
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        class Configuration
+        {
+            private readonly int min;
+            private readonly int max;
+
+            public Configuration(int v1, int v2)
+            {
+                min = v1;
+                max = v2;
+            }
+            public void ChangeMax(int newMax)
+            {
+                //max = newMax;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Configuration c = new(100,10);
+        }
+    }
+}
